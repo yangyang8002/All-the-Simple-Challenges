1be7f16a45c1d963f8a8cbe92b871b6da9c0bc5e
yangyang8002 <yangyang8002@qq.com>
2025-09-21 14:02:22 +0800

    1.0.2fix2

--- src/main/java/net/mcreator/allthesimplechallenges/command/OTPCommand.java
+++ src/main/java/net/mcreator/allthesimplechallenges/command/OTPCommand.java
@@ -20,7 +20,7 @@ public class OTPCommand {
     @SubscribeEvent
     public static void registerCommand(RegisterCommandsEvent event) {
         CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
-        
+
         dispatcher.register(Commands.literal("otp")
             .requires(source -> source.hasPermission(0))
             .then(Commands.argument("target", EntityArgument.player())
@@ -28,33 +28,30 @@ public class OTPCommand {
             .then(Commands.argument("pos", BlockPosArgument.blockPos())
                 .executes(context -> teleportToPos(context, BlockPosArgument.getLoadedBlockPos(context, "pos"))))
             .executes(context -> {
-                context.getSource().sendSystemMessage(Component.translatable("command.otp.usage"));
+                context.getSource().sendSystemMessage(Component.literal("Usage: /otp <player> or /otp <x y z>"));
                 return 0;
             })
         );
     }
 
-    private static int teleportToPlayer(CommandContext<CommandSourceStack> context, ServerPlayer target) 
+    private static int teleportToPlayer(CommandContext<CommandSourceStack> context, ServerPlayer target)
             throws CommandSyntaxException {
         ServerPlayer player = context.getSource().getPlayerOrException();
-        player.teleportTo(target.serverLevel(), target.getX(), target.getY(), target.getZ(), 
+        player.teleportTo(target.serverLevel(), target.getX(), target.getY(), target.getZ(),
                          target.getYRot(), target.getXRot());
         context.getSource().sendSystemMessage(
-            Component.translatable("command.otp.success.player", target.getDisplayName())
+            Component.literal("Teleported you to " + target.getDisplayName().getString() + ".")
         );
         return 1;
     }
 
-    private static int teleportToPos(CommandContext<CommandSourceStack> context, BlockPos pos) 
+    private static int teleportToPos(CommandContext<CommandSourceStack> context, BlockPos pos)
             throws CommandSyntaxException {
         ServerPlayer player = context.getSource().getPlayerOrException();
-        player.teleportTo(player.serverLevel(), pos.getX() + 0.5, pos.getY() + 1.0, pos.getZ() + 0.5, 
+        player.teleportTo(player.serverLevel(), pos.getX() + 0.5, pos.getY() + 1.0, pos.getZ() + 0.5,
                          player.getYRot(), player.getXRot());
         context.getSource().sendSystemMessage(
-            Component.translatable("command.otp.success.pos", 
-                String.valueOf(pos.getX()), 
-                String.valueOf(pos.getY()), 
-                String.valueOf(pos.getZ()))
+            Component.literal(String.format("Teleported you to %d, %d, %d.", pos.getX(), pos.getY(), pos.getZ()))
         );
         return 1;
     }
